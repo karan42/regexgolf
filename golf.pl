0d251715e43f30e59be71b18fd59c1c5be9bc0b2
Karan <karan@telehack.com>
2014-03-24 02:59:07 -0400

    golf.pl

--- golf.pl
+++ golf.pl
@@ -6,22 +6,27 @@ use warnings;
 
 my (@wordlist,@nomatch,@scores,$continue,$state);
 
-$continue = 1;
 print "\n\nHello Choose One:\n\n[1]Lessons\n[2]Game\n[3]Exit\n\nPlease Enter a Selection:";
 $continue= <STDIN>;
 
 while ($continue) {
+	print "while entry : $continue";
 	if ($continue==1){
 		print "You want to do lessons\n";
-		print "Enter a anther choice:";
+		print "Enter another choice 1:";
 		$continue=<STDIN>;
-		chomp $continue;
+		print "why is this not prining\n";
+		print "input from stid : $continue\n";
+		$state = inputsan (chomp($continue));
+		$continue = $state;
+		print "level 1 complete";
 	}
 	elsif ($continue==2) {
-		print "You want to play the game \n";
-		print "Enter a anther choice:";
+		print "You want to play the game \n$continue\n";
+		print "Enter a anther choice 2 :";
 		$continue=<STDIN>;
-		chomp $continue;
+		$continue = inputsan($continue);print $continue;
+	
 	}
 
 	
@@ -29,11 +34,25 @@ while ($continue) {
 		print "you chose exit, good bye!\n";
 		last;
 	}
+	elsif ($continue==42) {
+		print "WE are still searching for the question to that answer \n";
+		last;
+	}
 	else {
 		print "enter a valid selection:";
 		$continue = <STDIN>;
-		chomp $continue;
-
-
+		$continue = inputsan(chomp($continue));
 	}
+}
+
+
+sub inputsan {
+	my $selection = @_[0];
+	print join(",", @_);
+	chomp $selection;
+	if($selection !~ /\d{2}/) {
+		print "Please enter only a 2 digit number :";
+       		$selection= <STDIN>;
+ 		return $selection;		
+}
 }
